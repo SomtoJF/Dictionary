df1b74ade1004aebdb770f00410bcdc6e074e1c8
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-09-26 22:36:44 +0100

    Add form submit event listener

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import './index.css';
-import display from './screen';
+import "./index.css";
+import display from "./screen";
+import getDefinition from "./API";
+const form = <HTMLFormElement>document.getElementById("search-form");
+const input = <HTMLInputElement>document.getElementById("word-input");
 
 display.home();
+
+form.addEventListener("submit", (e) => {
+  getDefinition(input.value);
+  e.preventDefault();
+});
