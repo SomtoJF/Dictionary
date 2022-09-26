97362505bc060ad0cb9331d17e82651cd11e873f
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-09-26 21:58:37 +0100

    Refactor: Prettier format

--- src/screen.ts
+++ src/screen.ts
@@ -1,48 +1,42 @@
-let resultsContainer = document.getElementById('results');
+let resultsContainer = document.getElementById("results");
 
-type ScreenInterface = 
-{
-    node: HTMLElement;
-    clear: any;
-    error: any;
-    home :any;
-}
-
-function results(node:HTMLElement):ScreenInterface
-{
-    function clear()
-    {
-        node.innerHTML = "";
-    };
-
-    function error()
-    {
-        clear();
-        clearBackground();
-        node.classList.add('error');
-    };
-
-    function clearBackground()
-    {
-        clear();
-        node.classList.remove('error');
-        node.classList.remove('home');
-    }
-
-    function home()
-    {
-        clear();
-        clearBackground();
-        node.classList.add('home');
-    }
-
-    return{
-        node,
-        clear,
-        error,
-        home
-    };
+type ScreenInterface = {
+  node: HTMLElement;
+  clear: any;
+  error: any;
+  home: any;
 };
 
+function results(node: HTMLElement): ScreenInterface {
+  function clear() {
+    node.innerHTML = "";
+  }
+
+  function error() {
+    clear();
+    clearBackground();
+    node.classList.add("error");
+  }
+
+  function clearBackground() {
+    clear();
+    node.classList.remove("error");
+    node.classList.remove("home");
+  }
+
+  function home() {
+    clear();
+    clearBackground();
+    node.classList.add("home");
+  }
+
+  return {
+    node,
+    clear,
+    error,
+    home,
+  };
+}
+
 const display = results(resultsContainer);
-export default display ;
+export default display;
