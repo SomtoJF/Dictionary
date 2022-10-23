bb5f0d68268cb17a07ddf2a1d73461bf8779ef39
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-10-23 02:48:50 +0100

    Refactor: Rewrite code to take in a list of strings to be used as textcontent of multiple similar elements

--- src/DOM.ts
+++ src/DOM.ts
@@ -1,10 +1,16 @@
-function createContainer(content:string):HTMLElement
+function createContainers(content:Array<string>, parentElement: HTMLElement, elementToGenerate:string)
 {
-    let container = document.createElement('div');
-    container.textContent = content;
-    return container;
-}
+    let elementList = [];
+    for(let i = 0; i < content.length; i++)
+    {
+        let element = document.createElement(elementToGenerate);
+        element.textContent = content[i];
+        elementList[elementList.length] = element;
+        parentElement.appendChild(element);
+    };
+    return elementList;
+};
 
 export {
-    createContainer
-}
+    createContainers
+};
