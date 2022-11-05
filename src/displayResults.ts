7d4b3bd0dbc22ba402b3131ab0957397587320a5
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-11-05 15:47:14 +0100

    Write functions to filter out definitions from API response

--- src/displayResults.ts
+++ src/displayResults.ts
@@ -4,27 +4,56 @@ function show(text:string, container:HTMLElement):HTMLElement
     return container;
 };
 
+// displayDefinitions() takes an array of definitions, iterates through it and displays them in list tags <li>
+
+function displayDefinitions(array:any):any
+{
+  let definitionContainer = document.getElementById('definitionList');
+  definitionContainer.innerHTML = '';
+  array.forEach((item:string) => {
+    let def = show(item, document.createElement('li'));
+    definitionContainer.appendChild(def);
+  });
+};
+
 function sortResponse(list:any)
 {
+    const partsOfSpeech = getPartsOfSpeech(list);
+    const definition = getDefinitions(list);
+    function getDefinitions(list:Array<any>):Array<Array<string>>
+    {
+        let array = [];
+        for(let i = 0; i < list[0].meanings.length; i++)
+        {
+            array[i] = [];
+            for(let j = 0; j < list[0].meanings[i].definitions.length; j++)
+            {
+                array[i][j] = list[0].meanings[i].definitions[j].definition;
+            };
+        };
+        console.log(array);
+        return array;
+    };
     function getPartsOfSpeech(list:any):Array<string>
     {
         const resultArray = [];
-        for(let i = 0; i < list.length; i++)
+        for(let i = 0; i < list[0].meanings.length; i++)
         {
-            resultArray[resultArray.length] = list[0].meanings[0].partOfSpeech;
+            resultArray[resultArray.length] = list[0].meanings[i].partOfSpeech;
         };
         return resultArray;
-    }
-    const partsOfSpeech = getPartsOfSpeech(list);
+    };
     return{
         Name: list[0].word,
         Sound: list[0].phonetic,
-        PartsOfSpeech: partsOfSpeech
+        PartsOfSpeech: partsOfSpeech,
+        Definitions: definition
     };
 };
 
 
 export {
     sortResponse,
-    show
+    show,
+    displayDefinitions
 };
