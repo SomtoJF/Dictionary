d74713c5098c49971bdf1342936d63714fd24d33
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-11-05 23:02:44 +0100

    Add eventListeners to buttons to display parts of speech

--- src/screen.ts
+++ src/screen.ts
@@ -1,11 +1,11 @@
 let resultsContainer = document.getElementById("results");
 const information = document.getElementById("information");
-import {sortResponse, show} from './displayResults'
+import {sortResponse, show, displayDefinitions} from './displayResults';
 import {createContainers} from './DOM';
 
 type ScreenInterface = {
   node: HTMLElement;
-  clear: any;
+  clear: any ;
   error: any;
   home: any;
   results: any;
@@ -30,12 +30,12 @@ function results(node: HTMLElement): ScreenInterface {
     hide();
     clearBackground();
     node.classList.add("error");
-    node.innerHTML = '<div>word not found<div>';
+    // node.innerHTML = '<div>word not found<div>';
   }
 
   function clearBackground() {
-    node.classList.remove("error");
-    node.classList.remove("home");
+    node.classList.remove('error');
+    node.classList.remove('home');
   }
 
   function home() {
@@ -50,13 +50,28 @@ function results(node: HTMLElement): ScreenInterface {
     let speechContainer = document.getElementById('speech');
 
     const response = sortResponse(object);
-    unhide();
     clearBackground();
+    unhide();
     show(response.Name, wordContainer);
     show(response.Sound, soundContainer);
     speechContainer.innerHTML = '';
-    createContainers(response.PartsOfSpeech, speechContainer , 'button');
-  }
+    let partsOfSpeech:Array<HTMLElement> = createContainers(response.PartsOfSpeech, speechContainer , 'button');
+    displayDefinitions(response.Definitions[0]);
+
+    for(let i = 0; i < partsOfSpeech.length; i++)
+    {
+      partsOfSpeech[i].addEventListener('click', ()=>{
+        displayDefinitions(response.Definitions[i]);
+
+        partsOfSpeech.forEach((item)=>{
+          item.style.backgroundColor = 'transparent';
+        });
+
+        partsOfSpeech[i].style.backgroundColor = '#6c63ff';
+      });
+    };
+
+  };
 
   return {
     node,
